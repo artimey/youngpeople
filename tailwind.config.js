b51ec92993c815861971e709b74fe520b864d15d
plajboi <lyskov.2000@mai.ru>
2023-01-18 01:10:29 +0300

    hotfix/colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white": "#FFFFFF",
+        "white80": "rgba(255, 255, 255, 0.8)",
+        "white50": "rgba(255, 255, 255, 0.5)",
+        "white16": "rgba(255, 255, 255, 0.16)",
+        "white8": "rgba(255, 255, 255, 0.08)",
+        "black": "#1F1F1F",
+        "black50": "rgba(31, 31, 31, 0.5)",
+        "black80": "rgba(31, 31, 31, 0.8)",
+        "black16": "rgba(31, 31, 31, 0.16)",
+        "black8": "rgba(31, 31, 31, 0.08)",
+        "brandPink": "#E72978",
+        "brandBlue": "#332784",
+        "lightBlue": "#493BA8",
+        "errorRed": "#DA4545",
+        "successGreen": "#51DA45",
+      }
+    },
   },
   plugins: [],
 }
