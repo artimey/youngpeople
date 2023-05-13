b9848a150fa92ef8869ec76141aba726c8556cb4
arthur <atak23z@yandex.ru>
2023-05-13 15:57:07 +0300

    fix: bugs 1,6,7,8,12 event card transformer

--- src/components/Pages/AboutUs/Office.jsx
+++ src/components/Pages/AboutUs/Office.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import image1 from '../../../img/about/DSC01650.jpg';
-import image2 from '../../../img/about/DSC01646.jpg';
+import image1 from "../../../img/about/DSC01650.jpg";
+import image2 from "../../../img/about/DSC01646.jpg";
 
-import { ReactComponent as Mic } from '../../../img/about/mic.svg';
-import { ReactComponent as Cam } from '../../../img/about/cam.svg';
-import { ReactComponent as Laptop } from '../../../img/about/laptop.svg';
-import { ReactComponent as People } from '../../../img/about/people.svg';
-import { ReactComponent as Run } from '../../../img/about/run.svg';
-import { ReactComponent as Rocks } from '../../../img/about/rocks.svg';
+import { ReactComponent as Mic } from "../../../img/about/mic.svg";
+import { ReactComponent as Cam } from "../../../img/about/cam.svg";
+import { ReactComponent as Laptop } from "../../../img/about/laptop.svg";
+import { ReactComponent as People } from "../../../img/about/people.svg";
+import { ReactComponent as Run } from "../../../img/about/run.svg";
+import { ReactComponent as Rocks } from "../../../img/about/rocks.svg";
 
-import styles from './style.module.scss';
+import styles from "./style.module.scss";
 
 export const Office = () => {
   return (
@@ -29,35 +29,35 @@ export const Office = () => {
       </span>
 
       <div className={`px-5 sm:px-[41px] ${styles.container}`}>
-        <div className={styles.content}>
-          <span>
-            <Mic /> Зал для мероприятий
-          </span>
-          <span>
+        <ul className={styles.content}>
+          <li className={styles.contentItem}>
+            <Mic /> <span>Зал для мероприятий</span>
+          </li>
+          <li className={styles.contentItem}>
             <Cam />
-            Видеостудия
-          </span>
-          <span>
+            <span>Видеостудия</span>
+          </li>
+          <li className={styles.contentItem}>
             <Laptop />
-            Коворкинг
-          </span>
-          <span>
-            <Run /> Зал для тренировок и танцев
-          </span>
-          <span>
-            <People /> Переговорка
-          </span>
-          <span>
+            <span>Коворкинг</span>
+          </li>
+          <li className={styles.contentItem}>
+            <Run /> <span>Зал для тренировок и танцев</span>
+          </li>
+          <li className={styles.contentItem}>
+            <People /> <span>Переговорка</span>
+          </li>
+          <li className={styles.contentItem}>
             <Rocks />
             <span>
               Контент-ферма — все для работы, организации мероприятий, форумов,
               фестивалей и других событий, которые проводят общественные
               организации
             </span>
-          </span>
-        </div>
+          </li>
+        </ul>
         <Link to={`/co-working-zones/`} className={styles.button}>
-          Посмотреть <br/> пространства
+          Посмотреть <br /> пространства
         </Link>
       </div>
     </div>
--- src/utils/transformers/events.js
+++ src/utils/transformers/events.js
@@ -1,52 +1,59 @@
 import { transformNewsDate } from "../helpers";
 
 const datesTransformer = (dates) => {
-  const mounthEvent = (array, mounth) =>
-    array.every((item) => item.includes(mounth));
-  const fullYearEvent = (array, year) =>
-    array.every((item) => item.includes(year));
   if (dates) {
-    const d = dates.map((item) => {
-      return transformNewsDate(item);
+    const [dateStart, dateEnd] = dates.map((item) => {
+      const [day, month, year] = transformNewsDate(item).split(" ");
+      return { day, month, year };
     });
-    const [date, mounth, year] = d[0].split(" ");
-    if (fullYearEvent(d, year)) {
-      if (mounthEvent(d, mounth)) {
-        return `${date} - ${d[1].split(" ")[0]} ${mounth}`;
-      }
-      return d.map((item) => item.split(" ").slice(0, 2).join(" ")).join(" - ");
+    if (!dateEnd) {
+      return transformNewsDate(dates[0]);
     }
-    return d.join(" - ");
+    if (dateStart.month === dateEnd.month) {
+      return `${dateStart.day} - ${dateEnd.day} ${dateStart.month}`;
+    }
+    if (dateStart.month !== dateEnd.month) {
+      return `${dateStart.day} ${dateStart.month} - ${dateEnd.day} ${dateEnd.month}`;
+    }
   }
   return null;
 };
 
 export const eventsTransformer = (events) => {
   const url = "https://mosmolodezh.ru";
-
   if (events.data) {
-    return Object.keys(events.data).map((item) => ({
-      id: events.data[item]["Fields"]["ID"],
-      slug: events.data[item]["Fields"]["CODE"],
-      title: events.data[item]["Fields"]["NAME"],
-      img: url + events.data[item]["Fields"]["PREVIEW_PICTURE"],
-      hasImg: events.data[item]["Fields"]["PREVIEW_PICTURE"],
-      detailImg: url + events.data[item]["Fields"]["DETAIL_PICTURE"],
-      hasDetailImg: events.data[item]["Fields"]["DETAIL_PICTURE"],
-      dateStart: datesTransformer(
-        events.data[item]["Props"]["DATE_START"]["VALUE"]
-      ),
-      org: events.data[item]["Props"]["ORGANIZATION"]["VALUE_DETAIL"],
-      orgLogo:
-        url + events.data[item]["Props"]["ORGANIZATION"]["VALUE_DETAIL_PIC"],
-      slidersImage: events.data[item]["Props"]["SLIDER"]["VALUE"]
-        ? events.data[item]["Props"]["SLIDER"]["VALUE"].map(
-            (item) => url + item
-          )
-        : false,
-      place: events.data[item]["Props"]["ADDRESS"]["VALUE"] ?? '',
-      html: events.data[item]["Fields"]["DETAIL_TEXT"],
-    }));
+    return Object.keys(events.data).map((item) => {
+      try {
+        const event = {
+          id: events.data[item]["Fields"]["ID"],
+          slug: events.data[item]["Fields"]["CODE"],
+          title: events.data[item]["Fields"]["NAME"],
+          img: url + events.data[item]["Fields"]["PREVIEW_PICTURE"],
+          hasImg: events.data[item]["Fields"]["PREVIEW_PICTURE"],
+          detailImg: url + events.data[item]["Fields"]["DETAIL_PICTURE"],
+          hasDetailImg: events.data[item]["Fields"]["DETAIL_PICTURE"],
+          dateStart: events.data[item]["Props"]["DATE_START"]["VALUE"]
+            ? datesTransformer(
+                events.data[item]["Props"]["DATE_START"]["VALUE"]
+              )
+            : "",
+          org: events.data[item]["Props"]["ORGANIZATION"]["VALUE_DETAIL"],
+          orgLogo:
+            url +
+            events.data[item]["Props"]["ORGANIZATION"]["VALUE_DETAIL_PIC"],
+          slidersImage: events.data[item]["Props"]["SLIDER"]["VALUE"]
+            ? events.data[item]["Props"]["SLIDER"]["VALUE"].map(
+                (item) => url + item
+              )
+            : false,
+          place: events.data[item]["Props"]["ADDRESS"]["VALUE"] ?? "",
+          html: events.data[item]["Fields"]["DETAIL_TEXT"],
+        };
+        return event;
+      } catch (e) {
+        console.log("e", e);
+      }
+    });
   }
 
   return [];
